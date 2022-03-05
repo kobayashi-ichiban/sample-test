fda76b9b220cdea45080897e350806fee2276e57
kosuke-ykm <kosuke26yamazaki@gmail.com>
2022-03-05 23:54:54 +0900

    Enum Lesson

--- JavaExercise/src/jp/Effective/WeightTable.java
+++ JavaExercise/src/jp/Effective/WeightTable.java
@@ -9,5 +9,11 @@ public class WeightTable {
         for (Planet p : Planet.values()) {
             System.out.printf("Weight on %s is %f%n", p,  p.surfaceWeight(mass));
         }
+
+        double x = Double.parseDouble("2");
+        double y = Double.parseDouble("4");
+        for (EnumAndAnnotation.Operation2 op : EnumAndAnnotation.Operation2.values()) {
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+        }
     }
 }
