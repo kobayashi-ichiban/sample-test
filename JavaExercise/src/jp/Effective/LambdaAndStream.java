a0a64b371ade832a586a2d688cee4865f2a08ae6
kosuke-ykm <kosuke26yamazaki@gmail.com>
2022-04-05 23:45:53 +0900

    lambda Lesson

--- JavaExercise/src/jp/Effective/LambdaAndStream.java
+++ JavaExercise/src/jp/Effective/LambdaAndStream.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.DoubleBinaryOperator;
 
 public class LambdaAndStream {
     public static void main(String[] args) {
@@ -21,8 +22,74 @@ public class LambdaAndStream {
             }
         });
 
+        words.add("ETV");
         // 関数オブジェクトとしてのラムダ式(無名関数置き換え)
         Collections.sort(words,
                 (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+        words.add("QVC");
+        Collections.sort(words, Comparator.comparingInt(String::length));
+
+        words.add("SUN");
+        words.sort(Comparator.comparingInt(String::length));
+    }
+    // 定数固有クラス本体とデータを持つ
+    public enum Operation {
+        PLUS("+") {
+            public double apply(double x, double y) {
+                return x + y;
+            }
+        },
+        MINUS("-") {
+            public double apply(double x, double y) {
+                return x - y;
+            }
+        },
+        TIMES("*") {
+            public double apply(double x, double y) {
+                return x * y;
+            }
+        },
+        DIVIDE("/") {
+            public double apply(double x, double y) {
+                return x / y;
+            }
+        };
+
+        private final String symbol;
+        Operation(String symbol) {
+            this.symbol = symbol;
+        }
+
+        @Override
+        public String toString() {
+            return symbol;
+        }
+
+        public abstract double apply(double x, double y);
+    }
+
+    public enum Operation2 {
+        PLUS("+", (x, y)  -> x + y),
+        MINUS("-", (x, y)  -> x - y),
+        TIMES("*", (x, y)  -> x * y),
+        DIVIDE("/", (x, y)  -> x / y);
+
+        private final String symbol;
+        private final DoubleBinaryOperator op;
+
+        Operation2(String symbol, DoubleBinaryOperator op) {
+            this.symbol = symbol;
+            this.op = op;
+        }
+
+        @Override
+        public String toString() {
+            return symbol;
+        }
+
+        public double apply(double x, double y) {
+            return op.applyAsDouble(x, y);
+        }
     }
 }
