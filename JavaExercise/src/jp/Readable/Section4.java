7622f2036d9b36cf93c0488bc546d4b741ebbb25
kosuke-ykm <kosuke26yamazaki@gmail.com>
2022-04-10 22:40:32 +0900

    Readable Code Lesson

---
+++ JavaExercise/src/jp/Readable/Section4.java
@@ -0,0 +1,25 @@
+package jp.Readable;
+
+import java.util.List;
+
+public class Section4 {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+    }
+
+    class StatsKeeper {
+        public void add(double d) {
+            d = d + 1.0;
+        };
+        double Average() {
+            return 4.0 / 1.5;
+        }
+
+        private List<Double> past_items;
+        int count;
+
+        double minimum;
+        double maximum;
+    }
+}
