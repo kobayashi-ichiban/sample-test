eef068a6fe64e4c7268105c043ea62fd1a5ca062
kosuke-ykm <kosuke26yamazaki@gmail.com>
2022-04-23 23:15:50 +0900

    Fe

---
+++ JavaExercise/src/jp/exercise/Cache.java
@@ -0,0 +1,21 @@
+package jp.exercise;
+
+public abstract class Cache {
+    public enum Policy {
+        FIFO, LRU;
+    }
+
+    static Cache createCache(Policy policy) {
+        switch (policy) {
+            case FIFO:
+                return new ListBasedCache.Fifo();
+            case LRU:
+                return new ListBasedCache.Lru();
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    abstract byte[] getCachedBlockData(int index);
+
+    abstract void cacheBlockData(int index, byte[] blockData);
+}
