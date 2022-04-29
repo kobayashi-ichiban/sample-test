d45cdc87eab81fd5740b115bc8af2ac903671bde
kosuke-ykm <kosuke26yamazaki@gmail.com>
2022-04-29 22:28:21 +0900

    python test

--- PyExercise/source/test/Test_SuikaGame.py
+++ PyExercise/source/test/Test_SuikaGame.py
@@ -3,12 +3,13 @@ import unittest
 import math
 import SuikaGame
 
+
 class TestSuikaGame(unittest.TestCase):
     """test class of SuikaGame.py
     """
-    
+
     def test_calc_distance(self):
-    # calc_distanceのテスト
+        # calc_distanceのテスト
         value_x1 = 2
         value_y1 = 5
         value_x2 = 1
@@ -17,9 +18,10 @@ class TestSuikaGame(unittest.TestCase):
         excepted_s = math.sqrt(5)
         excepted_f = math.sqrt(4)
         # モジュール.クラス.メソッド名
-        actual = SuikaGame.SuikaGame.calc_distance(value_x1,value_y1,value_x2,value_y2)
+        actual = SuikaGame.SuikaGame.calc_distance(value_x1, value_y1, value_x2, value_y2)
         self.assertEqual(excepted_s, actual)
         self.assertEqual(excepted_f, actual)
+
 
 if __name__ == "__main__":
     unittest.main()
